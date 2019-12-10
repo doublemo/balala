de95f90e22661b72e42d751ccd494f397e6e0e1f
435420057 <435420057@qq.com>
2019-12-10 17:16:23 +0800

    add ip.go

---
+++ cores/networks/ip.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2019 The balala Authors <https://github.com/doublemo/balala>
+
+// Package networks 网络处理
+package networks
+
+import (
+	"errors"
+	"net"
+)
+
+// LocalIP 获取本地IP地址
+func LocalIP() (net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok {
+			if !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+				ip := ipNet.IP.To4()
+				if ip[0] == 10 || ip[0] == 172 && (ip[1] >= 16 && ip[1] < 32) || ip[0] == 192 && ip[1] == 168 {
+					return ip, nil
+				}
+			}
+		}
+	}
+
+	return nil, errors.New("Failed to get ip address")
+}
